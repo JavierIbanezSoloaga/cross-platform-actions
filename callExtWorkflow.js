70b9b1207f472154c37c051f30f435a21272c6ee
JavierIbanezSoloaga <javier.ibanezsoloaga@telefonica.com>
2023-03-28 13:58:41 +0200

    First commit

---
+++ callExtWorkflow.js
@@ -0,0 +1,18 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+try {
+    // `who-to-call` input defined in action metadata file
+    const whoToCall = core.getInput('who-to-call', { required: true })
+    console.log("Calling " + whoToCall);
+
+    const token = core.getInput('token')
+    console.log("Token: " + token)
+
+
+
+    // TODO: wait for the workflow to end and recover the output
+} catch (error) {
+    core.setFailed(error.message);
+
+}
