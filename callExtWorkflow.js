1b4d5f5e41bd8a2160c960fe61b13db39eb8e73a
JavierIbanezSoloaga <javier.ibanezsoloaga@telefonica.com>
2023-03-31 12:26:16 +0200

    trying again when not recovering runs

--- callExtWorkflow.js
+++ callExtWorkflow.js
@@ -31,38 +31,39 @@ try {
     })
 
     let workflowID = ""
+    let runsFetched = false
+    while (runsFetched) {
+        let response = await octokit.request('GET /repos/{owner}/{repo}/actions/runs?created={run_date_filter}', {
+            owner: 'JavierIbanezSoloaga',
+            repo: whoToCall,
+            run_date_filter: run_date_filter,
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28'
+            }
 
-    // while (workflowID === "") {
-    let response = await octokit.request('GET /repos/{owner}/{repo}/actions/runs?created={run_date_filter}', {
-        owner: 'JavierIbanezSoloaga',
-        repo: whoToCall,
-        run_date_filter: run_date_filter,
-        headers: {
-            'X-GitHub-Api-Version': '2022-11-28'
-        }
+        })
 
-    })
-
-    if (response.data.total_count > 0) {
-        console.log("hay runs")
-        let targetJob = null
-        while (targetJob === null) {
-            for (let run of response.data.workflow_runs) {
-                console.log("hay jobs")
-                let jobs = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{id}/jobs', {
-                    owner: 'JavierIbanezSoloaga',
-                    repo: whoToCall,
-                    id: run['id']
-                })
-                if (jobs.data.jobs.every(job => job.steps.every(step => step.status === "completed"))) {
-                    console.log("Estan completos")
-                    targetJob = jobs.data.jobs.find(job => job.steps.find(step => step.name === id))
+        if (response.data.total_count > 0) {
+            runsFetched = true
+            console.log("hay runs")
+            let targetJob = null
+            while (targetJob === null) {
+                for (let run of response.data.workflow_runs) {
+                    console.log("hay jobs")
+                    let jobs = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{id}/jobs', {
+                        owner: 'JavierIbanezSoloaga',
+                        repo: whoToCall,
+                        id: run['id']
+                    })
+                    if (jobs.data.jobs.every(job => job.steps.every(step => step.status === "completed"))) {
+                        console.log("Estan completos")
+                        targetJob = jobs.data.jobs.find(job => job.steps.find(step => step.name === id))
+                    }
                 }
             }
+            console.log(targetJob)
         }
-        console.log(targetJob)
     }
-    // }
 
     // TODO: wait for the workflow to end and recover the output
 } catch (error) {
