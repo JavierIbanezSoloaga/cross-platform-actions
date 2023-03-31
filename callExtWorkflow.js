0914777dc52f4c0d8f66a3e161a2749d14c5daf6
JavierIbanezSoloaga <javier.ibanezsoloaga@telefonica.com>
2023-03-31 10:38:39 +0200

    what the duck is inside run

--- callExtWorkflow.js
+++ callExtWorkflow.js
@@ -46,11 +46,12 @@ try {
         console.log(response.data.total_count)
 
         if(response.data.total_count > 0){
-            console.log(response.data.workflow_runs)
+            // console.log(response.data.workflow_runs)
             for(run of response.data.workflow_runs){
-                let jobs = await octokit.request('GET {jobs_url}', {
-                    jobs_url: run['jobs_url']
-                })
+                console.log(run)
+                // let jobs = await octokit.request('GET {jobs_url}', {
+                //     jobs_url: run['jobs_url']
+                // })
                 
                 console.log(jobs)
             }
