12956c5bf209721553aa03934f65a054f7f2478c
JavierIbanezSoloaga <javier.ibanezsoloaga@telefonica.com>
2023-03-31 14:45:56 +0200

    a few improvements

--- callExtWorkflow.js
+++ callExtWorkflow.js
@@ -1,6 +1,10 @@
 import { Octokit } from 'octokit';
 import * as core from '@actions/core'
 
+function sleep(time) {
+    return new Promise(r => setTimeout(r, time));
+}
+
 try {
     // `who-to-call` input defined in action metadata file
     const whoToCall = core.getInput('who-to-call', { required: true })
@@ -30,7 +34,7 @@ try {
         }
     })
 
-    let workflowID = ""
+    const sleepTime = 3000
     let jobFound = false
     while (!jobFound) {
         let response = await octokit.request('GET /repos/{owner}/{repo}/actions/runs?created={run_date_filter}', {
@@ -46,34 +50,33 @@ try {
         let runs = response.data.workflow_runs.filter(r => r.name !== "Run Deploy Action")
         if (runs.length > 0) {
             console.log("hay runs")
-            console.log(runs)
 
             let targetJob = null
-            if (runs.every(run => run.status === "completed")) {
 
-                while (targetJob === null) {
-                    for (let run of runs) {
-                        let jobs = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{id}/jobs', {
-                            owner: 'JavierIbanezSoloaga',
-                            repo: whoToCall,
-                            id: run['id']
-                        })
-                        if (jobs.data.jobs.every(job => job.status === "completed")) {
-                            console.log("Estan completos")
-                            console.log(jobs.data.jobs)
-                            targetJob = jobs.data.jobs.find(job => job.steps.find(step => step.name === id))
-                            if (targetJob !== undefined) break
-                        } else {
-                            await new Promise(r => setTimeout(r, 3000));
-                        }
+            while (targetJob === null) {
+                let completedRuns = runs.filter( run => run.status === "completed")
+                for (let run of completedRuns) {
+                    let jobs = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{id}/jobs', {
+                        owner: 'JavierIbanezSoloaga',
+                        repo: whoToCall,
+                        id: run['id']
+                    })
+                    if (jobs.data.jobs.every(job => job.status === "completed")) {
+                        console.log("Estan completos")
+                        console.log(jobs.data.jobs)
+                        targetJob = jobs.data.jobs.find(job => job.steps.find(step => step.name === id))
+                        if (targetJob !== undefined) break
+                    } else {
+                        await sleep(sleepTime)
                     }
                 }
             }
+
             console.log(targetJob)
             jobFound = !(targetJob === undefined | targetJob === null)
         }
         if (!jobFound) {
-            await new Promise(r => setTimeout(r, 3000));
+            await sleep(sleepTime)
         }
     }
 
