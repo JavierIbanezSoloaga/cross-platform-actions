06e64859db57283f69a243045ff3aebcdb3b20e0
JavierIbanezSoloaga <javier.ibanezsoloaga@telefonica.com>
2023-03-31 09:13:14 +0200

    first try fetching list of runs

--- callExtWorkflow.js
+++ callExtWorkflow.js
@@ -7,43 +7,45 @@ try {
     console.log("Calling " + whoToCall);
 
     const token = core.getInput('token')
-    console.log("Token: " + token)
+    //console.log("Token: " + token)
 
-    const run_date_filter = Date.now()
+    const run_date_filter = new Date().toLocaleDateString()
     console.log(run_date_filter)
 
-    // const octokit = new Octokit({
-    //     auth: token
-    // })
-    
-    // const id = '1234'
-    // await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
-    //     owner: 'JavierIbanezSoloaga',
-    //     repo: whoToCall,
-    //     workflow_id: 'printID.yml',
-    //     ref: 'main',
-    //     inputs: {
-    //         id: id
-    //     },
-    //     headers: {
-    //         'X-GitHub-Api-Version': '2022-11-28'
-    //     }
-    // })
+    const octokit = new Octokit({
+        auth: token
+    })
 
-    // let workflowID = ""
+    const id = '1234'
+    await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+        owner: 'JavierIbanezSoloaga',
+        repo: whoToCall,
+        workflow_id: 'printID.yml',
+        ref: 'main',
+        inputs: {
+            id: id
+        },
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    })
 
-    // while(workflowID === ""){
-    //     await octokit.request('GET /repos/{owner}/{repo}/actions/runs?created=%3E{run_date_filter}', {
-    //         owner: 'JavierIbanezSoloaga',
-    //         repo: whoToCall,
-    //         ref: 'main',
-    //         run_date_filter: run_date_filter,
-    //         headers: {
-    //             'X-GitHub-Api-Version': '2022-11-28'
-    //         }
+    let workflowID = ""
 
-    //     })
-    // }
+    //while(workflowID === ""){
+    let response = await octokit.request('GET /repos/{owner}/{repo}/actions/runs?created=%3E{run_date_filter}', {
+        owner: 'JavierIbanezSoloaga',
+        repo: whoToCall,
+        ref: 'main',
+        run_date_filter: run_date_filter,
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+
+    })
+
+    console-log(response)
+    //}
 
     // TODO: wait for the workflow to end and recover the output
 } catch (error) {
