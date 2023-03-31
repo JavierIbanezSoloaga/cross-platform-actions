6db653e7f170773ee96b80b6b4187d1679894127
JavierIbanezSoloaga <javier.ibanezsoloaga@telefonica.com>
2023-03-31 08:58:40 +0200

    adding get call

--- callExtWorkflow.js
+++ callExtWorkflow.js
@@ -9,25 +9,41 @@ try {
     const token = core.getInput('token')
     console.log("Token: " + token)
 
-    const octokit = new Octokit({
-        auth: token
-    })
+    const run_date_filter = Date.now()
+    console.log(run_date_filter)
+
+    // const octokit = new Octokit({
+    //     auth: token
+    // })
     
-    const id = '1234'
-    await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
-        owner: 'JavierIbanezSoloaga',
-        repo: whoToCall,
-        workflow_id: 'printID.yml',
-        ref: 'main',
-        inputs: {
-            id: id
-        },
-        headers: {
-            'X-GitHub-Api-Version': '2022-11-28'
-        }
-    })
+    // const id = '1234'
+    // await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+    //     owner: 'JavierIbanezSoloaga',
+    //     repo: whoToCall,
+    //     workflow_id: 'printID.yml',
+    //     ref: 'main',
+    //     inputs: {
+    //         id: id
+    //     },
+    //     headers: {
+    //         'X-GitHub-Api-Version': '2022-11-28'
+    //     }
+    // })
 
+    // let workflowID = ""
 
+    // while(workflowID === ""){
+    //     await octokit.request('GET /repos/{owner}/{repo}/actions/runs?created=%3E{run_date_filter}', {
+    //         owner: 'JavierIbanezSoloaga',
+    //         repo: whoToCall,
+    //         ref: 'main',
+    //         run_date_filter: run_date_filter,
+    //         headers: {
+    //             'X-GitHub-Api-Version': '2022-11-28'
+    //         }
+
+    //     })
+    // }
 
     // TODO: wait for the workflow to end and recover the output
 } catch (error) {
