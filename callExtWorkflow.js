b17fce12a3a6d02c8ccee1e5bca32de99a8b8d67
JavierIbanezSoloaga <javier.ibanezsoloaga@telefonica.com>
2023-04-03 13:43:36 +0200

    removing debug logs

--- callExtWorkflow.js
+++ callExtWorkflow.js
@@ -6,24 +6,30 @@ function sleep(time) {
 }
 
 try {
+    const SLEEP_DELAY = 3000
+    const workflow_id = 'printID.yml'
+    const owner = 'JavierIbanezSoloaga'
+    let targetJob = null
+    // TODO: Generate a random ID
+    const id = '1234'
+
     // `who-to-call` input defined in action metadata file
     const whoToCall = core.getInput('who-to-call', { required: true })
     console.log("Calling " + whoToCall);
 
     const token = core.getInput('token')
 
+    // Format YYYY-MM-DDTHH:MM
     const run_date_filter = new Date().toJSON().slice(0, 16)
-    console.log(run_date_filter)
 
     const octokit = new Octokit({
         auth: token
     })
-    // TODO: Generate a random ID
-    const id = '1234'
+    
     await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
-        owner: 'JavierIbanezSoloaga',
+        owner: owner,
         repo: whoToCall,
-        workflow_id: 'printID.yml',
+        workflow_id: workflow_id,
         ref: 'main',
         inputs: {
             id: id
@@ -33,11 +39,9 @@ try {
         }
     })
 
-    const sleepTime = 3000
-    let targetJob = null
     while (targetJob === undefined || targetJob === null) {
         let response = await octokit.request('GET /repos/{owner}/{repo}/actions/runs?created={run_date_filter}', {
-            owner: 'JavierIbanezSoloaga',
+            owner: owner,
             repo: whoToCall,
             run_date_filter: run_date_filter,
             headers: {
@@ -45,29 +49,25 @@ try {
             }
 
         })
-        console.log("here is a try")
         let runs = response.data.workflow_runs.filter(run => run.status === "completed")
         if (runs.length > 0) {
-            console.log("hay runs")
 
             for (let run of runs) {
                 let jobs = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{id}/jobs', {
-                    owner: 'JavierIbanezSoloaga',
+                    owner: owner,
                     repo: whoToCall,
                     id: run['id']
                 })
-                console.log(jobs.data.jobs)
                 targetJob = jobs.data.jobs.find(job => job.steps.find(step => step.name === id))
                 // If the target job is found go outside the loop 
                 if (targetJob !== undefined) break
             }
-
-            console.log(targetJob)
         }
         if (targetJob === undefined || targetJob === null) {
-            await sleep(sleepTime)
+            await sleep(SLEEP_DELAY)
         }
     }
+    console.log(targetJob)
 
     // TODO: wait for the workflow to end and recover the output
 } catch (error) {
